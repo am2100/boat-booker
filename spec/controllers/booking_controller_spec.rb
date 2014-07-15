cccbd9f5e0ba9afdb42b018401291ad36961ad71
am2100 <jimnoble@xjjz.co.uk>
2014-07-15 16:50:27 +0100

    Sketch out initial controller spec for #index action

---
+++ spec/controllers/booking_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe BookingController, :type => :controller do
+  describe 'GET #index' do
+    it 'calls the find method of the Booking model'
+
+    it 'makes an array of Bookings available to the view'
+
+    it 'renders the :index view'
+  end
+end
+
