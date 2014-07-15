a8c3bbc3f0b86f7cb1fcfd6a043397ac94bf995d
am2100 <jimnoble@xjjz.co.uk>
2014-07-15 16:56:50 +0100

    Controller spec: model call

--- spec/controllers/booking_controller_spec.rb
+++ spec/controllers/booking_controller_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 RSpec.describe BookingController, :type => :controller do
   describe 'GET #index' do
-    it 'calls the find method of the Booking model'
+    it 'calls the find method of the Booking model' do
+      fake_results = [double('booking_01'), double('booking_02'), double('booking_03')]
+      expect(Booking).to receive(:find).with(:all).and_return(fake_results)
+      get :index
+    end
 
     it 'makes an array of Bookings available to the view'
 
