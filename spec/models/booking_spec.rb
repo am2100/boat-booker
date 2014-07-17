8c4602227635f0030a21709e372d2e6ce10a0cc6
am2100 <jimnoble@xjjz.co.uk>
2014-07-17 07:14:22 +0100

    Add model specs

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Booking, :type => :model do
-  it 'is valid with a start and end time'
-  it 'is invalid without a start time'
+  it 'is valid with a start and end time' do
+    from = Time.now
+    to   = 1.hour.from_now
+    booking = Booking.new(from: from, to: to)
+    expect(booking).to be_valid
+    
+  end
+  it 'is invalid without a start time' do
+    expect(Booking.new(from: nil)).to have(1).errors_on(:from)
+  end
+
   it 'is invalid without an end time'
 end
