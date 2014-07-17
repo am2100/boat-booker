d487e58399822421795244061ef446ca7aedcd4f
am2100 <jimnoble@xjjz.co.uk>
2014-07-17 12:10:09 +0100

    Seed some bookings for view development

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+today = Date.today.to_time
+bookings_array = [{from: '6:00', to: '12:30'},{from: '12:30', to: '14:00'}]
+
+bookings_array.each do |booking|
+  from = Time.parse(booking[:from], today)
+  to   = Time.parse(booking[:to], today)
+  Booking.create!(from: from, to: to)
+end
