348cfdc693339ec5634bb19735f6d799aa5b7d94
am2100 <jimnoble@xjjz.co.uk>
2014-07-18 14:54:34 +0100

    Create helper methods to manage date and time formatting throughout the app. Update view to use new time/date helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def boat_booker_date(date)
+    p date.class
+    date.strftime("%d/%m/%Y")
+  end
+
+  def boat_booker_time(time)
+    time.strftime("%H:%M")
+  end
 end
