65f34e043976da8de2f16c5a9f54afd88b98a0bc
am2100 <jimnoble@xjjz.co.uk>
2014-07-19 17:20:30 +0100

    Add #create controller code

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,4 +6,13 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new
   end
+
+  def create
+    @booking = Booking.new(params[:booking])
+    if @booking.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
