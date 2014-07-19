85b069831473976664c215c1764bdbe5cef31848
am2100 <jimnoble@xjjz.co.uk>
2014-07-19 17:51:26 +0100

    Add flash[:notice] code

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,6 +10,7 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(params[:booking])
     if @booking.save
+      flash[:notice] = "Your booking was saved successfully"
       redirect_to root_path
     else
       render :new
