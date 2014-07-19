a736c91c0e391758a734de6a59debf2935edd210
am2100 <jimnoble@xjjz.co.uk>
2014-07-19 17:18:54 +0100

    Add invalid booking factory

--- spec/factories/bookings.rb
+++ spec/factories/bookings.rb
@@ -2,5 +2,10 @@ FactoryGirl.define do
   factory :booking do
     from Time.utc(2014, 07, 01, 10, 0)
     to   Time.utc(2014, 07, 01, 11, 0)
+
+    factory :invalid_booking do
+      from Time.utc(2014, 7, 1, 10, 0)
+      to   nil
+    end
   end
 end
