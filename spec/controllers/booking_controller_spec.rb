3e987a155aeda50e5b45095bfd7ded47bb4dca60
am2100 <jimnoble@xjjz.co.uk>
2014-07-21 13:49:36 +0100

    Refactor GET #index specs to use FactoryGirl

--- spec/controllers/booking_controller_spec.rb
+++ spec/controllers/booking_controller_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe BookingsController, :type => :controller do
   describe 'GET #index' do
     it 'calls the find method of the Booking model' do
-      fake_results = [double('booking_01'), double('booking_02'), double('booking_03')]
+      fake_results = [FactoryGirl.create(:booking)]
       expect(Booking).to receive(:find).with(:all).and_return(fake_results)
       get :index
     end
 
     it 'makes an array of Bookings available to the view' do
-      fake_results = [double('booking_01'), double('booking_02'), double('booking_03')]
+      fake_results = [FactoryGirl.create(:booking)]
       allow(Booking).to receive(:find).and_return(fake_results)
       get :index
       expect(assigns(:bookings)).to match_array(fake_results)
