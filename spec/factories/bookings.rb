38ca428eed4ae590133c7e124875e97d10710e87
am2100 <jimnoble@xjjz.co.uk>
2014-07-23 06:36:52 +0100

    Update Booking factory to use DateTime.

--- spec/factories/bookings.rb
+++ spec/factories/bookings.rb
@@ -1,11 +1,11 @@
 FactoryGirl.define do
   factory :booking do
-    book_from Time.utc(2014, 07, 01, 10, 0)
-    book_to   Time.utc(2014, 07, 01, 11, 0)
+    book_from DateTime.new(2014, 07, 01, 10, 0)
+    book_to   DateTime.new(2014, 07, 01, 11, 0)
 
     factory :booking_update do
-      book_from Time.utc(2014, 07, 01, 11, 0)
-      book_to   Time.utc(2014, 07, 01, 12, 0)
+      book_from DateTime.new(2014, 07, 01, 11, 0)
+      book_to   DateTime.new(2014, 07, 01, 12, 0)
     end
 
     factory :invalid_booking do
@@ -14,12 +14,12 @@ FactoryGirl.define do
     end
 
     factory :invalid_to_booking do
-      book_from Time.utc(2014, 7, 1, 10, 0)
+      book_from DateTime.new(2014, 7, 1, 10, 0)
       book_to   nil
     end
 
     factory :invalid_from_booking do
-      book_from Time.utc(2014, 7, 1, 10, 0)
+      book_from DateTime.new(2014, 7, 1, 10, 0)
       book_to   nil
     end
   end
