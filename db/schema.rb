899bfc08c436b07396e4e635fa50d34c6b740916
am2100 <jimnoble@xjjz.co.uk>
2014-07-25 15:35:47 +0100

    Add user column to bookings

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140725141033) do
+ActiveRecord::Schema.define(:version => 20140725142931) do
 
   create_table "bookings", :force => true do |t|
     t.datetime "book_from"
     t.datetime "book_to"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
