f5b80dc71c09bd7d26c1d0f9ffd3a75de3f63e9b
am2100 <jimnoble@xjjz.co.uk>
2014-07-26 10:24:36 +0100

    Add bcrypt gem and has_secure_password to User model

--- Gemfile
+++ Gemfile
@@ -37,3 +37,6 @@ end
 gem 'jquery-rails'
 gem 'haml-rails', '~> 0.4.0'
 
+# To use ActiveModel has_secure_password
+gem 'bcrypt-ruby', '~> 3.0.0'
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
+
   attr_accessible :name
 
   has_many :bookings
+
+  has_secure_password, validations: false
 
   validates :name, presence: true
   validates :name, uniqueness: true
