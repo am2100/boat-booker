5fd1ba80d6f1818aa3ecc64c9563fb8e3bdd7b8e
am2100 <jimnoble@xjjz.co.uk>
2014-07-27 11:33:18 +0100

    Keep has_secure_password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   has_many :bookings
 
-  has_secure_password, validations: false
+  has_secure_password
 
   validates :name, presence: true
   validates :name, uniqueness: true
