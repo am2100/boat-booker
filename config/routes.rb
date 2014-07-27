095cb04064cc4b086f7f1afaa73f55376131adbd
am2100 <jimnoble@xjjz.co.uk>
2014-07-27 13:29:13 +0100

    Correct typo. Add new routes to application layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 BoatBooker::Application.routes.draw do
-  get 'login' to: 'sessions#new', as: 'login'
-  get 'logout' to: 'sessions#destroy', as: 'logout'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :sessions
   resources :bookings
