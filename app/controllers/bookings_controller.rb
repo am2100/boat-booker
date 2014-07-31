3a0c060a9f2c7e8865f07bb0799a4ffc27297e23
am2100 <jimnoble@xjjz.co.uk>
2014-07-31 08:00:43 +0100

    Update controller to use new select params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,7 +9,22 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @booking = Booking.new(params[:booking])
+#raise params
+    year = DateTime.now.year
+p year
+    month = params[:date][:month].to_i
+p month
+    day = params[:date][:day].to_i
+p day
+    from = params[:book][:from].to_i
+p from
+    to = params[:book][:to].to_i
+p to
+
+    book_from = DateTime.new(year, month, day, from)
+    book_to   = DateTime.new(year, month, day, to)
+
+    @booking = Booking.new(book_from: book_from, book_to: book_to)
     if @booking.save
       flash[:notice] = "Your booking was saved successfully"
       redirect_to root_path
