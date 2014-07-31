397e8defde93b15afc199246c3f8bb3a8ac00e7e
am2100 <jimnoble@xjjz.co.uk>
2014-07-31 11:06:54 +0100

    Booking class returns month options according to season

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,18 @@
+require 'date'
+
 class Booking < ActiveRecord::Base
   attr_accessible :book_from, :book_to
 
   belongs_to :user
 
   validates :book_from, :book_to, presence: true
+
+  BOAT_SEASON = %w{ Mar Apr May Jun Jul Aug Sep Oct }
+
+  def self.month_options
+    options = []
+    BOAT_SEASON.each {|month| options << [ month, Date::ABBR_MONTHNAMES.index(month) ]}
+    return options
+  end
+
 end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -12,4 +12,20 @@ RSpec.describe Booking, :type => :model do
   it 'is invalid without an end time' do
     expect(FactoryGirl.build(:invalid_to_booking)).to_not be_valid
   end
+
+  it 'returns an array of month options' do
+    opts = Booking.month_options
+    expect(opts).to eq([['Mar', 3],
+                        ['Apr', 4],
+                        ['May', 5],
+                        ['Jun', 6],
+                        ['Jul', 7],
+                        ['Aug', 8],
+                        ['Sep', 9],
+                        ['Oct', 10]])
+  end
+
+  it 'returns an array of booking start time options'
+
+  it 'returns an array of booking end time options'
 end
