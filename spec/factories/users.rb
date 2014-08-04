1c395ca88814eb9dfc9ad4d5bc86b5b0c6cda705
am2100 <jimnoble@xjjz.co.uk>
2014-08-04 07:15:09 +0100

    Add :users_with_bookings factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,6 +5,24 @@ FactoryGirl.define do
     name "Bob"
     password "banana"
 
+    factory :user_with_bookings do
+      name 'Alice'
+      password 'banana'
+
+      # user_with_bookings will create booking data after the user has been created
+      transient do
+        bookings_count 5
+      end
+
+      # the after(:create) yields two values; the user instance itself and the
+      # evaluator, which stores all values from the factory, including transient
+      # attributes; `create_list`'s second argument is the number of records
+      # to create and we make sure the user is associated properly to the booking
+      after(:create) do |user, evaluator|
+        create_list(:booking, evaluator.bookings_count, user: user)
+      end
+    end
+
     factory :invalid_user do
       name nil
     end
