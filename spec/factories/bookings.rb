315fba0435ebebc045da1bb9dcc7d7f351c1e113
am2100 <jimnoble@xjjz.co.uk>
2014-08-16 07:48:32 +0100

    DRY out bookings factory

--- spec/factories/bookings.rb
+++ spec/factories/bookings.rb
@@ -7,37 +7,23 @@ FactoryGirl.define do
     factory :booking_update do
       book_from DateTime.new(2014, 07, 01, 11, 0)
       book_to   DateTime.new(2014, 07, 01, 12, 0)
-      user
     end
 
     factory :invalid_booking do
       book_from nil
       book_to   nil
-      user
     end
 
     factory :invalid_to_booking do
-      book_from DateTime.new(2014, 7, 1, 10, 0)
       book_to   nil
-      user
     end
 
     factory :invalid_from_booking do
       book_from   nil
-      book_to DateTime.new(2014, 7, 1, 10, 0)
-      user
     end
 
     factory :invalid_no_user_booking do
-      book_from DateTime.new(2014, 07, 01, 10, 0)
-      book_to   DateTime.new(2014, 07, 01, 11, 0)
       user nil
     end
-
-#    factory :invalid_non_existent_date_booking do
-#      book_from DateTime.new(2014, 02, 31, 10, 0)
-#      book_to DateTime.new(2014, 02, 31, 11, 0)
-#      user      
-#    end
   end
 end
